e4b08a28cc937ab1e918fcbc753267ec5245d02d
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-09-10 08:25:41 +0700

    Update donate.blade.php

--- resources/views/pages/donate.blade.php
+++ resources/views/pages/donate.blade.php
@@ -19,7 +19,7 @@
                             <span class="fa fa-hand-peace"></span>
                         </div>
                         <div class="cat-cap">
-                            <h5><a href="{{URL::to('/donate/transfer')}}">You can come directly to the office for donate</a></h5>
+                            <h5><a href="{{URL::to('/donate/transfer')}}" style="font-size: 24px;">You can come directly to the office for donate</a></h5>
                             <p>Address : Hà Nội, Việt Nam <br>
                                 Phone : +8880 888888 <br>
                                 Email : yasuo@gmail.com</p>
@@ -33,10 +33,10 @@
                         </div>
                         <div class="cat-cap">
                             <h5>@if(Session::get('events_id') != null)
-                                    <a href="{{URL::to('/donate/transfer/'.Session::get('events_id'))}}">Donate via account number</a>
+                                    <a href="{{URL::to('/donate/transfer/'.Session::get('events_id'))}}" style="font-size: 24px;">Donate via account number</a>
                                 @endif
                                 @if(Session::get('events_id') == null)
-                                <a href="{{URL::to('/donate/transfer')}}">Donate via account number</a>
+                                <a href="{{URL::to('/donate/transfer')}}" style="font-size: 24px;">Donate via account number</a>
                                 @endif
                             </h5>
                             <p>Card number: 0611001917137, <br>Card name: QUỸ TRÒ NGHÈO VÙNG CAO,
@@ -51,10 +51,10 @@
                         </div>
                         <div class="cat-cap">
                             @if(Session::get('events_id') != null)
-                                <a href="{{URL::to('/donate/donate_credit_card/'.Session::get('events_id'))}}">Donate by VNPAY</a>
+                                <a href="{{URL::to('/donate/donate_credit_card/'.Session::get('events_id'))}}" style="font-size: 24px;">Donate by VNPAY</a>
                             @endif
                             @if(Session::get('events_id') == null)
-                                <a href="{{URL::to('/donate/donate_credit_card')}}">Donate by VNPAY</a>
+                                <a href="{{URL::to('/donate/donate_credit_card')}}" style="font-size: 24px;">Donate by VNPAY</a>
                             @endif
                         </div>
                     </div>
